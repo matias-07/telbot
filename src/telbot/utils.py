24c177db30926763220845902fb3002acf13035a
Matias <mhuenul@fi.uba.ar>
2020-06-16 16:52:18 -0300

    Deleted utils.py

--- src/telbot/utils.py
+++
@@ -1,8 +0,0 @@
-def is_photo(content):
-	return len(content.split()) == 1 and any(map(
-		lambda e: content.endswith(f".{e}"),
-		["jpg", "png", "jpeg"]
-	))
-
-def is_animation(content):
-	return len(content.split()) == 1 and content.endswith(".gif")
